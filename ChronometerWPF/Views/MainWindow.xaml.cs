3c26fb561f1b9d7053f7db0cab431376e878f313
Ruben Aroca Mongaillard <ruben.aroca@col.vueling.com>
2020-03-19 11:46:30 +0100

    bind view

--- ChronometerWPF/Views/MainWindow.xaml.cs
+++ ChronometerWPF/Views/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ChronometerWPF.Models;
+using ChronometerWPF.ViewModels;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ChronometerWPF
 {
@@ -23,6 +12,7 @@ namespace ChronometerWPF
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new ChronometerViewModel(new Chronometer());
         }
     }
 }
