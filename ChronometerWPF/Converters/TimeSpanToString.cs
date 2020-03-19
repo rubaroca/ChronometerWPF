cc6d55404a6cc6ca17e66db492c27ac25a9058b2
Ruben Aroca Mongaillard <ruben.aroca@col.vueling.com>
2020-03-19 11:43:44 +0100

    add converter

---
+++ ChronometerWPF/Converters/TimeSpanToString.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace ChronometerWPF.Converters
+{
+    public class TimeSpanToString : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var timeSpan = (TimeSpan)value;
+            return timeSpan.ToString(@"hh\:mm\:ss", culture);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
