dacebac9786559581badc41093185e47793f5332
Ruben Aroca Mongaillard <ruben.aroca@col.vueling.com>
2020-03-19 10:50:59 +0100

    add baseViewModel for update property

---
+++ ChronometerWPF/ViewModels/BaseViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ChronometerWPF.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
